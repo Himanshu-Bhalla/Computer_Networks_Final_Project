e93febc6637de6cac61222eaf0e3161e350131be
Himanshu Bhalla <himanshubhalla@Himanshus-MacBook-Air.local>
2021-12-01 17:28:57 +0530

    final commit

---
+++ Mail_client/app.py
@@ -0,0 +1,151 @@
+from socket import *
+import ssl
+import base64
+import getpass
+#import OpenSSL
+from flask import Flask
+from flask import request
+import smtplib
+from email.mime.multipart import MIMEMultipart
+from email.mime.text import MIMEText
+from email.mime.base import MIMEBase
+from email import encoders
+
+app = Flask(__name__)
+
+@app.route("/send")
+
+def send_email():
+    receiver = request.args.get("receiver")
+    Subject = request.args.get("subject")
+    Text = request.args.get("body")
+    option = request.args.get("option")
+    filename = request.args.get("attach")
+
+
+    endmsg = "\r\n.\r\n"
+
+    if option == "no":
+
+        gooogleSMTPServer = 'smtp.gmail.com'
+        gooogleSMTPPort= 465
+
+        print ("creating the socket")
+        clientSocket = socket(AF_INET,SOCK_STREAM)
+        print ("connecting to the socket")
+
+        wrappedSocket = ssl.wrap_socket(clientSocket,
+                        ssl_version=ssl.PROTOCOL_TLSv1,
+                        ciphers="HIGH:-aNULL:-eNULL:-PSK:RC4-SHA:RC4-MD5",
+                        cert_reqs=ssl.CERT_REQUIRED)
+
+        wrappedSocket.connect((gooogleSMTPServer, gooogleSMTPPort))
+
+        print ("socket is connected")
+        recv = wrappedSocket.recv(1024)
+
+        print ("received msg is: %s" % recv)
+        if recv[:3] != '220':
+            print ('Remote server not responding to 220.')
+
+        Commandhelo = 'HELO Alice\r\n'
+        print Commandhelo
+        wrappedSocket.send(Commandhelo)
+        recv1 = wrappedSocket.recv(1024)
+        print "Received msg from recv1 is: %s" % recv1
+        if recv1[:3] != '250':
+            print 'Remote server not responding to 250.'
+
+        # you can enter your passwords in Username and Password
+        Username="gbsir87@gmail.com"
+        Password= "qwaszxerdfcv12#$"
+
+        UP=("\000"+Username+"\000"+Password).encode("base64")
+
+        print UP
+        UP=UP.strip("\n")
+        login = 'AUTH PLAIN '+ UP + '\r\n'
+        print login
+        wrappedSocket.send(login)
+        recv_from_TLS = wrappedSocket.recv(1024)
+        print recv_from_TLS
+
+        fromCommand = 'MAIL FROM: <'+ Username+'>\r\n'
+        print fromCommand
+        wrappedSocket.send(fromCommand)
+        recv2 = wrappedSocket.recv(1024)
+        print "Received msg from recv1 is: %s" % recv2
+        if recv2[:3] != '250':
+            print('rcpt2 to 250 reply not received from server, cabron.')
+
+        toCommand = 'RCPT TO: <'+ receiver +'>\r\n'
+        print toCommand
+        wrappedSocket.send(toCommand)
+        recv3 = wrappedSocket.recv(1024)
+        print "Reply received from server 3 is : %s" % recv3
+        if recv3[:3] != '250':
+            print('rcpt3 to 250 reply not received from server, cabron.')
+
+        dataCommand = 'DATA\r\n'
+        print dataCommand
+        wrappedSocket.send(dataCommand)
+        recv4 = wrappedSocket.recv(1024)
+        print recv4
+        wrappedSocket.send("Subject: "+Subject+"\r\n\r\n"+Text+"\r\n"+ endmsg +"\r\n")
+        recv5 = wrappedSocket.recv(1024)
+        print recv5
+
+        wrappedSocket.send("QUIT\r\n")
+        recv6 = wrappedSocket.recv(1024)
+        print recv6
+        wrappedSocket.close()
+
+        return "done"
+    else:
+        # you can enter your username in fromaddr
+
+        fromaddr = "gbsir87@gmail.com"
+        toaddr = receiver
+
+        msg = MIMEMultipart()
+
+        msg['From'] = fromaddr
+
+        msg['To'] = toaddr
+
+        msg['Subject'] = Subject
+
+        body = Text
+
+        msg.attach(MIMEText(body, 'plain'))
+
+
+        attachment = open(filename, "rb")
+
+        p = MIMEBase('application', 'octet-stream')
+
+        p.set_payload((attachment).read())
+
+        encoders.encode_base64(p)
+
+        p.add_header('Content-Disposition', "attachment; filename= %s" % filename)
+
+        msg.attach(p)
+
+        s = smtplib.SMTP('smtp.gmail.com', 587)
+
+        s.starttls()
+        # you can enter your password in s.login() after fromaddr
+
+        s.login(fromaddr, "qwaszxerdfcv12#$")
+
+        text = msg.as_string()
+
+        s.sendmail(fromaddr, toaddr, text)
+
+        s.quit()
+
+        return "done"
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0')
---
+++ README.md
@@ -0,0 +1,57 @@
+# Email Client
+In this project we created an Email client using python and websockets. The additional feature added is that it can also send attachments.
+
+# Steps to run the code
+Before following the below steps download the full zip code from github. You can use any compiler but I have shown you the process using pycharm.
+
+## Step1
+Open the Pycharm and in the bottom right corner click on add interpreter and then a new window will open there click on new "Inherit global site-packages" and then give name to your virtual environment.
+Initially we have given venv but in your machine u will have to add another.
+Below are the images of both windows.
+![Adding interpreter](https://i.imgur.com/ONiyguo.png)
+![Adding interpreter 2](https://i.imgur.com/t8X8MP1.png)
+
+## Step 2
+Enter the username and password of the account from which you want to send mail on line 59,60,104,136.
+![Username and password](https://i.imgur.com/4L5oauP.png)
+
+## Step 3
+Open the pycharm terminal and enter command "pip install flask" to download flask.Given below is the screenshot and the command.
+```aidl
+pip install flask
+```
+![installing flask](https://i.imgur.com/0qISAUW.png)
+
+## Step4
+Now to run server thpe the command "python -m flask run" to run the deployment server.Given below is the screenshot and the command.
+```aidl
+python -m flask run
+```
+![Server](https://i.imgur.com/S5u2t8V.png)
+
+## Step 5
+Now open index.html file from the downloaded zip file and open that file using Google Chrome.I have attached the screenshot of the homepage of the mail client. Now 
+in the mail client you will have to type whether you want to send attachment or not. Then fill all the details.
+![Server](https://i.imgur.com/ZGT22XG.png)
+
+### Below is the screenshot of mail send without attachment
+![Server](https://i.imgur.com/KpO5JbA.png)
+
+Screenshot of sent side.
+![Server](https://i.imgur.com/vjgfUJB.jpg)
+
+Screenshot of received mail.
+![Server](https://i.imgur.com/i7mWIZS.jpg)
+
+### Below is the screenshot of mail send with attachment
+![Server](https://i.imgur.com/4vETz76.png)
+
+Screenshot of sent side.
+![Server](https://i.imgur.com/RH7xchr.jpg)
+
+Screenshot of received mail.
+![Server](https://i.imgur.com/RXFDQVb.jpg)
+
+# Future work and contact details
+
+We would lik
